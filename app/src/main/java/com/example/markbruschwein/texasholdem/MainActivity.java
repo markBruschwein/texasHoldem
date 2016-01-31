0a3a1e1c369c5dccf38d6b4bba2f54a419130dad
markbruschwein <bruschwe18@up.edu>
2016-01-31 15:42:26 -0800

    HomeWork 4c Commit

--- app/src/main/java/com/example/markbruschwein/texasholdem/MainActivity.java
+++ app/src/main/java/com/example/markbruschwein/texasholdem/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.markbruschwein.texasholdem;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
+
+    protected SeekBar betSeekBar = null;
+    protected TextView betTextView = null;
+    protected LinearLayout card1 = null;
+    protected LinearLayout card2 = null;
+    protected boolean card1Check = false;
+    protected boolean card2Check = false;
+    protected LinearLayout handCard1 = null;
+    protected LinearLayout handCard2 = null;
+    protected TextView userCard1ValueTop = null;
+    protected TextView userCard1ValueSuit = null;
+    protected TextView userCard1ValueBottom = null;
+    protected TextView userCard2ValueTop = null;
+    protected TextView userCard2ValueSuit = null;
+    protected TextView userCard2ValueBottom = null;
+    protected Button foldButton = null;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +50,27 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        betSeekBar = (SeekBar)findViewById(R.id.betSeekBar);
+        betSeekBar.setOnSeekBarChangeListener(this);
+
+        betTextView = (TextView)findViewById(R.id.betTextView);
+
+        card1 = (LinearLayout)findViewById(R.id.card1);
+        card1.setOnClickListener(this);
+        card2 = (LinearLayout)findViewById(R.id.card2);
+        card2.setOnClickListener(this);
+
+        handCard1 = (LinearLayout)findViewById(R.id.handCard1);
+        handCard2 = (LinearLayout)findViewById(R.id.handCard2);
+        userCard1ValueBottom = (TextView)findViewById(R.id.userCard1ValueBottom);
+        userCard1ValueSuit = (TextView)findViewById(R.id.userCard1Suit);
+        userCard1ValueTop = (TextView)findViewById(R.id.userCard1ValueTop);
+        userCard2ValueBottom = (TextView)findViewById(R.id.userCard2ValueBottom);
+        userCard2ValueSuit = (TextView)findViewById(R.id.userCard2Suit);
+        userCard2ValueTop = (TextView)findViewById(R.id.userCard2ValueTop);
+        foldButton = (Button)findViewById(R.id.foldButton);
+        foldButton.setOnClickListener(this);
     }
 
     @Override
@@ -49,4 +94,66 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v.getId() == R.id.card1)
+        {
+            if(card1Check == false)
+            {
+                this.card1.setBackgroundColor(0xFFFFFF4D);
+                card1Check = true;
+            }
+            else
+            {
+                this.card1.setBackgroundColor(0xFFFCFBFB);
+                card1Check = false;
+            }
+        }
+        else if(v.getId() == R.id.card2)
+        {
+            if(card2Check == false)
+            {
+                this.card2.setBackgroundColor(0xFFFFFF4D);
+                card2Check = true;
+            }
+            else
+            {
+                this.card2.setBackgroundColor(0xFFFCFBFB);
+                card2Check = false;
+            }
+        }
+        else if(v.getId() == R.id.foldButton) {
+            this.userCard1ValueTop.setTextColor(0xFF000000);
+            this.userCard1ValueSuit.setTextColor(0xFF000000);
+            this.userCard1ValueBottom.setTextColor(0xFF000000);
+            this.userCard2ValueTop.setTextColor(0xFF000000);
+            this.userCard2ValueSuit.setTextColor(0xFF000000);
+            this.userCard2ValueBottom.setTextColor(0xFF000000);
+            this.handCard1.setBackgroundColor(0xFF000000);
+            this.handCard2.setBackgroundColor(0xFF000000);
+        }
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+    {
+        betTextView.setText("" + progress * 10);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar)
+    {
+        //not used
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar)
+    {
+        //not used
+    }
+
+
 }
