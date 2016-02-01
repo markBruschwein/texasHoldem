fd2831db605f79d780ceea7c8f33d11ba93f1237
markbruschwein <bruschwe18@up.edu>
2016-01-31 22:51:53 -0800

    Updated Card Selecting

--- app/src/main/java/com/example/markbruschwein/texasholdem/MainActivity.java
+++ app/src/main/java/com/example/markbruschwein/texasholdem/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,12 +17,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
+    //instance variables for custom betting
     protected SeekBar betSeekBar = null;
     protected TextView betTextView = null;
+
+    // cards in the middle
     protected LinearLayout card1 = null;
     protected LinearLayout card2 = null;
+    protected LinearLayout card3 = null;
+    protected LinearLayout card4 = null;
+    protected LinearLayout card5 = null;
+
+    // instance variables related to the user's hand
     protected boolean card1Check = false;
     protected boolean card2Check = false;
+    protected boolean card3Check = false;
+    protected boolean card4Check = false;
+    protected boolean card5Check = false;
+    protected boolean handCard1Check = false;
+    protected boolean handCard2Check = false;
     protected LinearLayout handCard1 = null;
     protected LinearLayout handCard2 = null;
     protected TextView userCard1ValueTop = null;
@@ -31,6 +45,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     protected TextView userCard2ValueSuit = null;
     protected TextView userCard2ValueBottom = null;
     protected Button foldButton = null;
+
+
+    protected int numCardsSelected = 0;
 
 
 
@@ -56,11 +73,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
         betTextView = (TextView)findViewById(R.id.betTextView);
 
-        card1 = (LinearLayout)findViewById(R.id.card1);
-        card1.setOnClickListener(this);
-        card2 = (LinearLayout)findViewById(R.id.card2);
-        card2.setOnClickListener(this);
 
+
+        // for handling the event that fold is selected
         handCard1 = (LinearLayout)findViewById(R.id.handCard1);
         handCard2 = (LinearLayout)findViewById(R.id.handCard2);
         userCard1ValueBottom = (TextView)findViewById(R.id.userCard1ValueBottom);
@@ -71,6 +86,21 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         userCard2ValueTop = (TextView)findViewById(R.id.userCard2ValueTop);
         foldButton = (Button)findViewById(R.id.foldButton);
         foldButton.setOnClickListener(this);
+
+
+        // for all face up cards
+        handCard1.setOnClickListener(this);
+        handCard2.setOnClickListener(this);
+        card1 = (LinearLayout)findViewById(R.id.card1);
+        card1.setOnClickListener(this);
+        card2 = (LinearLayout)findViewById(R.id.card2);
+        card2.setOnClickListener(this);
+        card3 = (LinearLayout)findViewById(R.id.card3);
+        card3.setOnClickListener(this);
+        card4 = (LinearLayout)findViewById(R.id.card4);
+        card4.setOnClickListener(this);
+        card5 = (LinearLayout)findViewById(R.id.card5);
+        card5.setOnClickListener(this);
     }
 
     @Override
@@ -98,33 +128,119 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onClick(View v)
     {
+        // the next five if else statements are for
+        // highlighting cards, but no more than five can be chosen at once
         if(v.getId() == R.id.card1)
         {
-            if(card1Check == false)
+            if(card1Check == false && numCardsSelected < 5)
             {
                 this.card1.setBackgroundColor(0xFFFFFF4D);
                 card1Check = true;
+                numCardsSelected++;
             }
-            else
+            else if(card1Check == true)
             {
                 this.card1.setBackgroundColor(0xFFFCFBFB);
                 card1Check = false;
+                numCardsSelected--;
             }
         }
         else if(v.getId() == R.id.card2)
         {
-            if(card2Check == false)
+            if(card2Check == false && numCardsSelected < 5)
             {
                 this.card2.setBackgroundColor(0xFFFFFF4D);
                 card2Check = true;
+                numCardsSelected++;
             }
-            else
+            else if(card2Check == true)
             {
                 this.card2.setBackgroundColor(0xFFFCFBFB);
                 card2Check = false;
+                numCardsSelected--;
             }
         }
-        else if(v.getId() == R.id.foldButton) {
+        else if(v.getId() == R.id.card3)
+        {
+            if(card3Check == false && numCardsSelected < 5)
+            {
+                this.card3.setBackgroundColor(0xFFFFFF4D);
+                card3Check = true;
+                numCardsSelected++;
+            }
+            else if(card3Check == true)
+            {
+                this.card3.setBackgroundColor(0xFFFCFBFB);
+                card3Check = false;
+                numCardsSelected--;
+            }
+        }
+        else if(v.getId() == R.id.card4)
+        {
+            if(card4Check == false && numCardsSelected < 5)
+            {
+                this.card4.setBackgroundColor(0xFFFFFF4D);
+                card4Check = true;
+                numCardsSelected++;
+            }
+            else if(card4Check == true)
+            {
+                this.card4.setBackgroundColor(0xFFFCFBFB);
+                card4Check = false;
+                numCardsSelected--;
+            }
+        }
+        else if(v.getId() == R.id.card5)
+        {
+            if(card5Check == false && numCardsSelected < 5)
+            {
+                this.card5.setBackgroundColor(0xFFFFFF4D);
+                card5Check = true;
+                numCardsSelected++;
+            }
+            else if(card5Check == true)
+            {
+                this.card5.setBackgroundColor(0xFFFCFBFB);
+                card5Check = false;
+                numCardsSelected--;
+            }
+        }
+        else if(v.getId() == R.id.handCard1)
+        {
+            if(handCard1Check == false && numCardsSelected < 5)
+            {
+                this.handCard1.setBackgroundColor(0xFFFFFF4D);
+                handCard1Check = true;
+                numCardsSelected++;
+            }
+            else if(handCard1Check == true)
+            {
+                this.handCard1.setBackgroundColor(0xFFFCFBFB);
+                handCard1Check = false;
+                numCardsSelected--;
+            }
+        }
+        else if(v.getId() == R.id.handCard2)
+        {
+            if(handCard2Check == false && numCardsSelected < 5)
+            {
+                this.handCard2.setBackgroundColor(0xFFFFFF4D);
+                handCard2Check = true;
+                numCardsSelected++;
+            }
+            else if(handCard2Check == true)
+            {
+                this.handCard2.setBackgroundColor(0xFFFCFBFB);
+                handCard2Check = false;
+                numCardsSelected--;
+            }
+        }
+
+
+        else if(v.getId() == R.id.foldButton)
+        {
+            // if the user hits the fold button, both of their cards turn black
+
             this.userCard1ValueTop.setTextColor(0xFF000000);
             this.userCard1ValueSuit.setTextColor(0xFF000000);
             this.userCard1ValueBottom.setTextColor(0xFF000000);
@@ -135,6 +251,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
             this.handCard2.setBackgroundColor(0xFF000000);
         }
 
+        Log.i("Num of selected cards", "" + numCardsSelected);
     }
 
     @Override
